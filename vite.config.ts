7f623e9c407b69c591ba0179f86877c909198d95
arjun182739183929283928328398 <supersonicarjun123@gmail.com>
2025-07-16 20:24:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Seed-Haven/',
+  base: '/Seed-Haven/', // repo name
   plugins: [react()],
 })
